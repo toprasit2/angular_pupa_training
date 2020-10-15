5bea93e08a8773d255ce7a9d5d7bde0bc625899e
SUPAKRIT AREEWATTANA <supakrit_aree@hotmail.com>
2020-10-15 18:41:43 +0700

    do it again

--- client/src/app/home/home.component.spec.ts
+++ client/src/app/home/home.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { HomepageComponent } from './homepage.component';
+import { HomeComponent } from './home.component';
 
-describe('HomepageComponent', () => {
-  let component: HomepageComponent;
-  let fixture: ComponentFixture<HomepageComponent>;
+describe('HomeComponent', () => {
+  let component: HomeComponent;
+  let fixture: ComponentFixture<HomeComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomepageComponent ]
+      declarations: [ HomeComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(HomepageComponent);
+    fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- client/src/app/home/home.module.ts
+++ client/src/app/home/home.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomepageComponent } from './homepage/homepage.component';
+import { HomeComponent } from './home.component';
 
 
 
 @NgModule({
-  declarations: [HomepageComponent],
+  declarations: [HomepageComponent, HomeComponent],
   imports: [
     CommonModule
   ]
