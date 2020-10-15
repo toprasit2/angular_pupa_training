ed511a2a3fcf11ac74589d4cff438218e7c61673
SUPAKRIT AREEWATTANA <supakrit_aree@hotmail.com>
2020-10-15 18:39:48 +0700

    create home module & component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ButtonModule } from 'primeng/button';
+import { from } from 'rxjs';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ButtonModule],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule, HomeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
